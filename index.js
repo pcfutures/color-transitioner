0088430b2070db6a50102df904ba8e5173448e75
SalemPCF <salem.cresswell@btinternet.com>
2019-01-24 13:34:13 +0000

    Updated comments

--- index.js
+++ index.js
@@ -8,15 +8,18 @@ class ColorTransitioner {
      * @param {Number} delay - The time before the transition starts
      * @param {Number} amount - The amount to increment/decrement everytime the color transitions
      * @param {Function} cb - The callback, called with the updated color every time it transitions
-     * Usage: const transitioner = new ColorTransitioner(
-     *      false,
-     *      { r: 255, g: 255, b: 255 },
-     *      { r: 0, g: 0, b: 0 },
-     *      1,
-     *      0,
-     *      1,
-     *      ({ r, g, b }) => console.log(r, g, b)),
-     *  );
+     * Usage:
+     *     const transitioner = new ColorTransitioner(
+     *         false,
+     *         { r: 255, g: 255, b: 255 },
+     *         { r: 0, g: 0, b: 0 },
+     *         1,
+     *         0,
+     *         1,
+     *         ({ r, g, b }) => console.log(r, g, b)),
+     *     );
+     *
+     *     transitioner.transition();
      */
     constructor (custom, from, to, interval = 1, delay = 0, amount = 1, cb = () => null) {
         this.isCustom = custom;
